fad34b00ee60780f16e303e770b61f2f43ff5002
abidex4yemi <abidex4yemi@gmail.com>
2020-04-07 08:29:42 +0100

    Update: simplenodeserver app

--- simpleNodeServer/index.js
+++ simpleNodeServer/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi, welcome to the root');
+    res.send('Hi, it is working....');
 });
 
 app.listen(2020, console.log('Listening on port 2020'));
